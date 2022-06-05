9bfd3d6e554e2e46e9f8d6e573e9ad8db401221d
Anthony Simmon <simmon.anthony@gmail.com>
2022-06-05 00:32:00 -0400

    Added Contains, StartsWith and EndsWith attribute tests

---
+++ src/GSoft.ComponentModel.DataAnnotations.Tests/ContainsAttributeTests.cs
@@ -0,0 +1,44 @@
+using Xunit;
+
+namespace GSoft.ComponentModel.DataAnnotations.Tests;
+
+public class ContainsAttributeTests
+{
+    [Fact]
+    public void IsValid_Returns_True_When_Value_Is_Null()
+    {
+        var attr = new ContainsAttribute("whatever");
+        Assert.True(attr.IsValid(null));
+    }
+
+    [Fact]
+    public void IsValid_Returns_False_When_Value_Is_Not_A_String()
+    {
+        var attr = new ContainsAttribute("whatever");
+        Assert.False(attr.IsValid(new object()));
+    }
+
+    [Fact]
+    public void IsValid_Returns_False_When_Value_Not_Found()
+    {
+        var attr = new ContainsAttribute("42");
+        Assert.False(attr.IsValid("foobar"));
+        Assert.False(attr.IsValid("FOOBAR"));
+    }
+
+    [Fact]
+    public void IsValid_Works_Case_Sensitive()
+    {
+        var attr = new ContainsAttribute("oo");
+        Assert.True(attr.IsValid("foobar"));
+        Assert.False(attr.IsValid("FOOBAR"));
+    }
+
+    [Fact]
+    public void IsValid_Works_Case_Insensitive()
+    {
+        var attr = new ContainsAttribute("oo") { IgnoreCase = true };
+        Assert.True(attr.IsValid("foobar"));
+        Assert.True(attr.IsValid("FOOBAR"));
+    }
+}
--- src/GSoft.ComponentModel.DataAnnotations.Tests/StringExtensions.cs
+++ src/GSoft.ComponentModel.DataAnnotations.Tests/StringExtensions.cs
@@ -4,15 +4,6 @@ namespace GSoft.ComponentModel.DataAnnotations.Tests;
 
 internal static class StringExtensions
 {
-    public static string FormatInvariant(this string format, object? arg)
-        => string.Format(CultureInfo.InvariantCulture, format, arg);
-
-    public static string FormatInvariant(this string format, object? arg0, object? arg1)
-        => string.Format(CultureInfo.InvariantCulture, format, arg0, arg1);
-
-    public static string FormatInvariant(this string format, object? arg0, object? arg1, object? arg2)
-        => string.Format(CultureInfo.InvariantCulture, format, arg0, arg1, arg2);
-
     public static string FormatInvariant(this string format, params object?[] args)
         => string.Format(CultureInfo.InvariantCulture, format, args);
 }
